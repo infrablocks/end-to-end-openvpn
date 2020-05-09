f4f3710244f9bb907fd6e62b15f7681463344369
Toby Clemson <tobyclemson@gmail.com>
2020-05-10 00:39:41 +0100

    Add network terraform configuration.

---
+++ infra/network/network.tf
@@ -0,0 +1,15 @@
+data "aws_availability_zones" "all" {}
+
+module "base_network" {
+  source = "infrablocks/base-networking/aws"
+  version = "2.4.0"
+
+  component = var.component
+  deployment_identifier = var.deployment_identifier
+
+  vpc_cidr = var.vpc_cidr
+  region = var.region
+  availability_zones = data.aws_availability_zones.all.names
+
+  private_zone_id = data.terraform_remote_state.domain.outputs.private_zone_id
+}
