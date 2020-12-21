3498375e680f50f8abaea94d25eb6ff094859535
Toby Clemson <tobyclemson@gmail.com>
2020-12-21 02:48:50 +0000

    Allow storage bucket to be destroyed when objects present.

--- infra/bootstrap/bucket.tf
+++ infra/bootstrap/bucket.tf
@@ -1,8 +1,10 @@
-module "state_bucket" {
+module "storage_bucket" {
   source = "infrablocks/encrypted-bucket/aws"
   version = "1.5.0"
 
   bucket_name = var.storage_bucket_name
+
+  allow_destroy_when_objects_present = "yes"
 
   tags = {
     DeploymentIdentifier = var.deployment_identifier
